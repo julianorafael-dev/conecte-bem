8e13273f7aec304c45f1659303a8895505d2abf1
julianorafael-dev <julianorafa15@hotmail.com>
2026-09-11 18:39:28 -0300

    feat: estrutura páginas e estilos globais

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,6 @@
 import Header from './componentes/Header/Header.jsx'
 import Footer from './componentes/Footer/Footer.jsx'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 
 function App() {
   
@@ -7,8 +8,14 @@ function App() {
   return (
     <>
     <Header />
-
-
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/cadastro" element={<Cadastro />} />
+        <Route path="/perfil" element={<Perfil />} />
+      </Routes>
+    </BrowserRouter>
     <Footer />
     </>
   )
