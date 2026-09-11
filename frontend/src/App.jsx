3eb93d033ac568bb34f21a60971127c2163d8ff5
julianorafael-dev <julianorafa15@hotmail.com>
2026-09-11 18:18:42 -0300

    feat: adiciona componente footer

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,16 @@
 import Header from './componentes/Header/Header.jsx'
+import Footer from './componentes/Footer/Footer.jsx'
 
 function App() {
   
 
   return (
+    <>
     <Header />
+
+
+    <Footer />
+    </>
   )
 }
 
