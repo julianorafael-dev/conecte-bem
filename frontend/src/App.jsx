3fe925d0866367d51c8017431b5c50b9de5dec2c
julianorafael-dev <julianorafa15@hotmail.com>
2026-09-23 20:36:17 -0300

    feat: ajusta header, footer e app para mobile

---
+++ frontend/src/App.jsx
@@ -0,0 +1,13 @@
+import Footer from './componentes/Footer/Footer.jsx'
+import Header from './componentes/Header/Header.jsx'
+
+function App() {
+  return (
+    <>
+      <Header />
+      <Footer />
+    </>
+  )
+}
+
+export default App
